a5d1c583a7887a5dc5543d09d15473b7992eef1e
Suni <suni.suni571@gmail.com>
2018-01-26 09:03:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function placeOrder(cardNumber) {
    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
  }
 }
-
+console.log(placeOreder(57892705874));
 
 
 
