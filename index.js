ce847e16855c6a8b2fb95cf34766664cdfd9d5cd
Suni <suni.suni571@gmail.com>
2018-01-26 08:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ console.log(total());
 function removeFromCart(item) {
   for(let j=0; j< cart.length; j++){
              if(cart[j].hasOwnProperty(item) === true){
-              Object.keys(cart[j]);
+              
              }
              }
   }
