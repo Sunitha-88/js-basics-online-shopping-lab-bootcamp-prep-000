fbbdd37de471c0995d2fb4179baca94a617af50b
Suni <suni.suni571@gmail.com>
2018-01-26 09:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,13 +73,15 @@ function removeFromCart(item) {
   return cart;
 }
 console.log(removeFromCart('Apple'));
+
+
 function placeOrder(cardNumber) {
  if(cardNumber === undefined){
    return "Sorry, we don't have a credit card on file for you.";
  }
  else{
    cart=[];
-   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
  }
 }
 console.log(placeOrder(57892705874));
